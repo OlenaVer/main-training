cb06747ad8fe2cccaf687cd585c7f6994b279221
OlenaVer <7702ukr@ukr.net>
2022-05-24 12:26:17 +0300

    JS-5-lesson-100%

--- lesson-JS-5/mine.js
+++ lesson-JS-5/mine.js
@@ -111,3 +111,123 @@ let filter=users.filter(function (item) {
 });
 console.log(filter);
 console.log(users.filter(value => value.status));
+//_______________________________________ незрозуміло нижче
+// function filterArray(array,callback){
+//     let container=[];
+//     for (const item of container) {
+//         if (callback()){
+//             container.push(item)
+//         }
+//     }
+//     return container;
+// }
+// console.log(filterArray(users));
+//________________________________
+function filterArray(array) {
+    let container=[];
+    for (const item of array) {
+       if (item.age<30){
+           container.push(item)
+       }
+    }
+    return container;
+}
+console.log(filterArray(users));
+//__________________________________________
+// function filterArray1(array) {
+//     let container1=[];
+//     for (const item of array) {
+//         let result= checkout (item);
+//         if (item.age<30){
+//             container1.push(item)
+//         }
+//     }
+//     return container1;
+// }
+// console.log(filterArray1(users));
+//______________________________________
+
+function filterArray2(array2, checkout) {
+    let conteiner2=[];
+    for (const item2 of array2) {
+      let result=checkout(item2);
+      if (result){
+          conteiner2.push(item2)
+      }    }
+    return conteiner2;
+}
+
+console.log(filterArray2(users, function (item2) {
+    return item2.age < 30;
+}));
+//те, що зверху можна записати:
+function filterArray3(array3, checkout) {
+    let container3=[];
+    for (const item3 of array3) {
+        let result=function (item3) {
+            return item3.age < 30;
+        };
+        if (result){
+            container3.push(item3)
+        }    }
+    return container3;
+}
+
+console.log(filterArray3(users, function (item3) {
+    return item3.age < 30;
+}));
+
+//функція рекурсив, реккурсії , функція яка сама себе викликає
+
+function rec(q) {
+  q++;
+  console.log(q);
+    if (q>4){return;
+   }
+    rec(q);
+}
+rec(0);
+//__________________________наступна працює внизу
+function rec1(e,limit) {
+    e++;
+    console.log(e);
+    if (e>limit){return;
+    }
+    rec1(e,limit);
+}
+rec1(0,12);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
